4b7bebeee111cc00d37ca6ac87c92660ae00e043
daniel <asa47972000@naver.com>
2022-10-08 03:10:59 +1000

    123

--- src/client/main.js
+++ src/client/main.js
@@ -18,13 +18,19 @@ const sceneArr = [
 		heightMultiple: 5,
 		obj: {
 			scene: document.querySelector('.scene0'),
-			canvas: document.querySelector('.scene0 .video'),
+			v1: document.querySelector('.scene0 .video'),
 			message1: document.querySelector('.scene0 .message_1'),
 			message2: document.querySelector('.scene0 .message_2'),
 			message3: document.querySelector('.scene0 .message_3'),
 		},
 		// stR enR stV edV
 		value: {
+			v1: {
+				opacityIn: [0, 0.1, 0, 1],
+				opacityOut: [0.9, 1, 1, 0],
+				threshold: 0.5,
+				SR: 0,
+			},
 			// message 1 (0.1 ~ 0.3)
 			m1: {
 				opacityIn: [0.1, 0.15, 0, 1],
@@ -59,12 +65,61 @@ const sceneArr = [
 		heightMultiple: 5,
 		obj: {
 			scene: document.querySelector('.scene1'),
-			canvas: document.querySelector('.scene1 .video'),
-			message1: document.querySelector('.scene1 message_1'),
-			message2: document.querySelector('.scene1 message_2'),
-			message3: document.querySelector('.scene1 message_3'),
-			message4: document.querySelector('.scene1 message_4'),
-			message5: document.querySelector('.scene1 message_5'),
+			v2: document.querySelector('.scene1 .video_elem'),
+			message1: document.querySelector('.scene1 .message_1'),
+			message2: document.querySelector('.scene1 .message_2'),
+			message3: document.querySelector('.scene1 .message_3'),
+			message4: document.querySelector('.scene1 .message_4'),
+			message5: document.querySelector('.scene1 .message_5'),
+		},
+		value: {
+			// for Video
+			v2: {
+				opacityIn: [0, 0.1, 0, 1],
+				opacityOut: [0.9, 1, 1, 0],
+				threshold: 0.5,
+				SR: 0,
+			},
+			// message 1 (0.05 ~ 0.2)
+			m1: {
+				opacityIn: [0.05, 0.1, 0, 1],
+				opacityOut: [0.15, 0.2, 1, 0],
+				translateYIn: [0.05, 0.1, RangeForTransY, 0],
+				translateYOut: [0.15, 0.2, 0, -RangeForTransY],
+				threshold: 0.13,
+			},
+			// message 2 (0.23 ~ 0.38)
+			m2: {
+				opacityIn: [0.23, 0.28, 0, 1],
+				opacityOut: [0.33, 0.38, 1, 0],
+				translateYIn: [0.23, 0.28, RangeForTransY, 0],
+				translateYOut: [0.33, 0.38, 0, -RangeForTransY],
+				threshold: 0.31,
+			},
+			// message 3 (0.41 ~ 0.56)
+			m3: {
+				opacityIn: [0.41, 0.46, 0, 1],
+				opacityOut: [0.51, 0.56, 1, 0],
+				translateYIn: [0.41, 0.46, RangeForTransY, 0],
+				translateYOut: [0.51, 0.56, 0, -RangeForTransY],
+				threshold: 0.48,
+			},
+			// message 4 (0.59 ~ 0.74)
+			m4: {
+				opacityIn: [0.59, 0.64, 0, 1],
+				opacityOut: [0.69, 0.74, 1, 0],
+				translateYIn: [0.59, 0.64, RangeForTransY, 0],
+				translateYOut: [0.69, 0.74, 0, -RangeForTransY],
+				threshold: 0.67,
+			},
+			// message 5(0.77 ~ 0.92)
+			m5: {
+				opacityIn: [0.77, 0.82, 0, 1],
+				opacityOut: [0.87, 0.92, 1, 0],
+				translateYIn: [0.77, 0.82, RangeForTransY, 0],
+				translateYOut: [0.87, 0.92, 0, -RangeForTransY],
+				threshold: 0.9,
+			},
 		},
 	},
 	// Scene 2
@@ -74,32 +129,69 @@ const sceneArr = [
 		heightMultiple: 10,
 		obj: {
 			scene: document.querySelector('.scene2'),
-			canvas1: document.querySelector('.video_1'),
-			message1: document.querySelector('video1_message1'),
-			message2: document.querySelector('video1_message2'),
-			message3: document.querySelector('video1_message3'),
-			canvas2: document.querySelector('.video_2'),
-			message4: document.querySelector('video2_message1'),
-			message5: document.querySelector('video2_message2'),
-			message6: document.querySelector('video2_message3'),
-			message7: document.querySelector('video2_message4'),
-			canvas3: document.querySelector('.video_3'),
-			message8: document.querySelector('video3_message1'),
-			message9: document.querySelector('video3_message2'),
-			message10: document.querySelector('video3_message3'),
-			message11: document.querySelector('video3_message4'),
-			// Overpainting pictures
-			// messages for video_4, but still use the canvas for video_3
-			message12: document.querySelector('video4_message1'),
-			message13: document.querySelector('video4_message2'),
-			message14: document.querySelector('video4_message3'),
-			message15: document.querySelector('video4_message4'),
+			v3: document.querySelector('.video_1'), // 0 ~ 0.2
+			message1: document.querySelector('.video1_message1'),
+			message2: document.querySelector('.video1_message2'),
+			message3: document.querySelector('.video1_message3'),
+			message4: document.querySelector('.video1_message4'),
+			message5: document.querySelector('.video1_message5'),
+			v4: document.querySelector('.video_2'), // 0.2 ~ 0.45
+			message6: document.querySelector('.video2_message1'),
+			message7: document.querySelector('.video2_message2'),
+			message8: document.querySelector('.video2_message3'),
+			message9: document.querySelector('.video2_message4'),
+			message10: document.querySelector('.video2_message5'),
+			v5: document.querySelector('.video_3'), // 0.5 ~ 0.75
+			message11: document.querySelector('.video3_message1'),
+			message12: document.querySelector('.video3_message2'),
+			message13: document.querySelector('.video3_message3'),
+			message14: document.querySelector('.video3_message4'),
+			message15: document.querySelector('.video3_message5'),
+			v6: document.querySelector('.video_4'), // 0.75 ~ 1
+		},
+		value: {
+			v3: {
+				opacityIn: [0, 0.1, 0, 1],
+				opacityOut: [0.5, 0.55, 1, 0],
+				threshold: 0.3,
+				SR: 0,
+				WR: 0
+			},
+			v4: {
+				threshold: 0.3,
+				rotateIn: 90,
+				rotateOut: 0,
+				// translateY(In, Out) will be adjusted after "setSize" method executed
+				translateYIn: 0,
+				translateYOut: 0,
+				SR: 0,
+				WR: 0,
+				fixedWidth: 0
+			},
+			v5: {
+				threshold: 0.3,
+				rotateIn: 90,
+				rotateOut: 0,
+				// translateY(In, Out) will be adjusted after "setSize" method executed
+				translateYIn: 0,
+				translateYOut: 0,
+				SR: 0,
+				WR: 0,
+				fixedWidth: 0
+			},
+			v6: {
+				// canvas_elem (0.75 ~ 1)
+				opacityIn: [0.55, 0.6, 0, 1],
+				SR: 0,
+				WR: 0
+			},
 		},
 	},
 ];
 
 const setSize = () => {
 	const innerH = window.innerHeight;
+	const bodyW = document.body.offsetWidth;
 	for (let i = 0; i < sceneArr.length; i++) {
 		// Set the height values of each scenes by the value 'heightMultiple'
 		sceneArr[i].height = innerH * sceneArr[i].heightMultiple;
@@ -109,6 +201,46 @@ const setSize = () => {
 
 	// Set canvas size
 	// canvas should cover the screen
+	// Arr -> for doing 'for' loop
+	const videoArr = [];
+	videoArr.push(sceneArr[0].obj.v1);
+	videoArr.push(sceneArr[1].obj.v2);
+	videoArr.push(sceneArr[2].obj.v3);
+	videoArr.push(sceneArr[2].obj.v4); // videoArr[3]
+	videoArr.push(sceneArr[2].obj.v5); // videoArr[4]
+	videoArr.push(sceneArr[2].obj.v6);
+	console.log(videoArr[1].offsetWidth, videoArr[1].width);
+	console.log(videoArr[1].offsetHeight, videoArr[1].height);
+	for (let i = 0; i < videoArr.length; i++) {
+		const HR = videoArr[i].offsetHeight / innerH;
+		const WR = videoArr[i].offsetWidth / bodyW;
+		const SR = HR < WR ? 1 / HR : 1 / WR;
+		videoArr[i].style.transform = `scale(${SR}) translate3d(-${
+			(1 / SR) * 50
+		}%, -${(1 / SR) * 50}%, 0)`;
+
+		// set SR
+		if (i === 0) sceneArr[i].value.v1.SR = SR;
+
+		if (i === 1) sceneArr[i].value.v2.SR = SR;
+
+		if (i > 1) {
+			sceneArr[2].value[`v${i + 1}`].SR = SR;
+			sceneArr[2].value[`v${i + 1}`].WR = WR;
+			console.log(WR);
+		}
+
+		// Scene2 -> adjust the location of videos(v4, v5)
+		sceneArr[2].value.v4.fixedWidth = sceneArr[2].obj.v4.offsetWidth * sceneArr[2].value.v4.WR;
+		sceneArr[2].value.v5.fixedWidth = sceneArr[2].obj.v5.offsetWidth * sceneArr[2].value.v5.WR;
+
+		videoArr[3].style.transfrom = `scale(${SR}) translate3d(-${
+			(1 / SR) * 50
+		}%, -${(1 / SR) * 100}%, 0)`;
+		videoArr[3].style.transfrom = `scale(${SR}) translate3d(-${
+			(1 / SR) * 50
+		}%, ${(1 / SR) * 100}%, 0)`;
+	}
 };
 
 const getCurrentSceneScrollY = () => {
@@ -204,7 +336,6 @@ const imgLoad = () => {
 		// push I_Arr in imgArr
 		imgArr.push(I_Arr);
 	}
-	console.log(imgArr);
 };
 
 // ** Play Animation !important
@@ -212,6 +343,7 @@ const playAnimaiton = (ratio) => {
 	// declare variables for playing animaiton
 	let op = 0;
 	let transY = 0;
+	let fixedY = 0;
 
 	// currentScene <- Global variable
 	const CVal = sceneArr[currentScene].value;
@@ -220,9 +352,17 @@ const playAnimaiton = (ratio) => {
 		case 0: // SCENE 0
 			// Img -> 271 // range of Img: 0 ~ 270
 			const imgIndex = Math.round(ratio * (ImgCount[currentScene] - 1));
-			const can = sceneArr[currentScene].obj.canvas;
+			const can = sceneArr[currentScene].obj.v1;
 			const cont = can.getContext('2d');
 			cont.drawImage(imgArr[currentScene][imgIndex], 0, 0);
+
+			// Video Opacity
+			if (ratio < CVal.v1.threshold) {
+				op = getValForElement(CVal.v1.opacityIn, ratio);
+			} else {
+				op = getValForElement(CVal.v1.opacityOut, ratio);
+			}
+			CObj.v1.style.opacity = op;
 
 			// Message 1
 			if (ratio < CVal.m1.threshold) {
@@ -232,8 +372,11 @@ const playAnimaiton = (ratio) => {
 				op = getValForElement(CVal.m1.opacityOut, ratio);
 				transY = getValForElement(CVal.m1.translateYOut, ratio);
 			}
+			fixedY = CObj.message1.offsetHeight / 2;
 			CObj.message1.style.opacity = op;
-			CObj.message1.style.transform = `translateY(${transY}px)`;
+			CObj.message1.style.transform = `translate3d(-50%,${
+				-fixedY + transY
+			}px, 0)`;
 
 			// Message 2
 			if (ratio < CVal.m2.threshold) {
@@ -243,8 +386,12 @@ const playAnimaiton = (ratio) => {
 				op = getValForElement(CVal.m2.opacityOut, ratio);
 				transY = getValForElement(CVal.m2.translateYOut, ratio);
 			}
+
+			fixedY = CObj.message2.offsetHeight / 2;
 			CObj.message2.style.opacity = op;
-			CObj.message2.style.transform = `translateY(${transY}px)`;
+			CObj.message2.style.transform = `translate3d(-50%,${
+				-fixedY + transY
+			}px, 0)`;
 
 			// Message 3
 			if (ratio < CVal.m3.threshold) {
@@ -254,12 +401,105 @@ const playAnimaiton = (ratio) => {
 				op = getValForElement(CVal.m3.opacityOut, ratio);
 				transY = getValForElement(CVal.m3.translateYOut, ratio);
 			}
+			fixedY = CObj.message3.offsetHeight / 2;
 			CObj.message3.style.opacity = op;
-			CObj.message3.style.transform = `translateY(${transY}px)`;
+			CObj.message3.style.transform = `translate3d(-50%,${
+				-fixedY + transY
+			}px, 0)`;
+
+			// video element in other scenes shouldn't be seen
+			sceneArr[1].obj.v2.style.opacity = 0;
 			break;
-		case 1:
+		case 1: // Scene 1
+			// Video Elem
+			if (ratio < CVal.v2.threshold) {
+				op = getValForElement(CVal.v2.opacityIn, ratio);
+			} else {
+				op = getValForElement(CVal.v2.opacityOut, ratio);
+			}
+			CObj.v2.style.opacity = op;
+
+			// Message 1
+			if (ratio < CVal.m1.threshold) {
+				op = getValForElement(CVal.m1.opacityIn, ratio);
+				transY = getValForElement(CVal.m1.translateYIn, ratio);
+			} else {
+				op = getValForElement(CVal.m1.opacityOut, ratio);
+				transY = getValForElement(CVal.m1.translateYOut, ratio);
+			}
+			fixedY = CObj.message1.offsetHeight / 2;
+			CObj.message1.style.opacity = op;
+			CObj.message1.style.transform = `translate3d(-50%,${
+				-fixedY + transY
+			}px, 0)`;
+			// Message 2
+			if (ratio < CVal.m2.threshold) {
+				op = getValForElement(CVal.m2.opacityIn, ratio);
+				transY = getValForElement(CVal.m2.translateYIn, ratio);
+			} else {
+				op = getValForElement(CVal.m2.opacityOut, ratio);
+				transY = getValForElement(CVal.m2.translateYOut, ratio);
+			}
+			fixedY = CObj.message2.offsetHeight / 2;
+			CObj.message2.style.opacity = op;
+			CObj.message2.style.transform = `translate3d(-50%,${
+				-fixedY + transY
+			}px, 0)`;
+			// Message 3
+			if (ratio < CVal.m3.threshold) {
+				op = getValForElement(CVal.m3.opacityIn, ratio);
+				transY = getValForElement(CVal.m3.translateYIn, ratio);
+			} else {
+				op = getValForElement(CVal.m3.opacityOut, ratio);
+				transY = getValForElement(CVal.m3.translateYOut, ratio);
+			}
+			fixedY = CObj.message3.offsetHeight / 2;
+			CObj.message3.style.opacity = op;
+			CObj.message3.style.transform = `translate3d(-50%,${
+				-fixedY + transY
+			}px, 0)`;
+			// Message 4
+			if (ratio < CVal.m4.threshold) {
+				op = getValForElement(CVal.m4.opacityIn, ratio);
+				transY = getValForElement(CVal.m4.translateYIn, ratio);
+			} else {
+				op = getValForElement(CVal.m4.opacityOut, ratio);
+				transY = getValForElement(CVal.m4.translateYOut, ratio);
+			}
+			fixedY = CObj.message4.offsetHeight / 2;
+			CObj.message4.style.opacity = op;
+			CObj.message4.style.transform = `translate3d(-50%,${
+				-fixedY + transY
+			}px, 0)`;
+			// Message 5
+			if (ratio < CVal.m5.threshold) {
+				op = getValForElement(CVal.m5.opacityIn, ratio);
+				transY = getValForElement(CVal.m5.translateYIn, ratio);
+			} else {
+				op = getValForElement(CVal.m5.opacityOut, ratio);
+				transY = getValForElement(CVal.m5.translateYOut, ratio);
+			}
+			fixedY = CObj.message5.offsetHeight / 2;
+			CObj.message5.style.opacity = op;
+			CObj.message5.style.transform = `translate3d(-50%,${
+				-fixedY + transY
+			}px, 0)`;
+
+			// videos in other scenes shouldn't be seen
+			sceneArr[0].obj.v1.style.opacity = 0;
 			break;
 		case 2:
+			// V3
+			if (ratio < CVal.v3.threshold) {
+				op = getValForElement(CVal.v3.opacityIn, ratio);
+			} else {
+				op = getValForElement(CVal.v3.opacityOut, ratio);
+			}
+			CObj.v3.style.opacity = op;
+
+			// videos in other scenes shouldn't be seen
+			sceneArr[0].obj.v1.style.opacity = 0;
+			sceneArr[1].obj.v2.style.opacity = 0;
 			break;
 		default:
 			break;
